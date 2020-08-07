d20e6aae777b6f4d7c85a78941f00f3e6b1cda50
Edwina Palmer <hekamara@gmail.com>
2020-08-07 14:07:16 -0400

    SC answers

---
+++ SC1/oop1.py
@@ -0,0 +1,27 @@
+class Vehicle():        #base class
+    pass          
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+
+
+
+
+
+  
