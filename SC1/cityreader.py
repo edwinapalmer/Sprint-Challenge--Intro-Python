22e85a186dfe1ce3710df779b2952e54e4b89e00
Edwina Palmer <hekamara@gmail.com>
2020-08-07 14:25:46 -0400

    SC answers

--- SC1/cityreader.py
+++ SC1/cityreader.py
@@ -10,7 +10,7 @@ class City():
         self.lon = lon
 
     def __str__(self):
-        return f"{self.name}: lat:{self.lat}, long:{self.lon}"
+        return f'"{self.name}", {self.lat}, {self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -33,15 +33,15 @@ def cityreader(cities=[]):
   # `cities` list
 
   rows = []
-  with open('cities.csv') as csvfile:
-      reader = csv.reader(csvfile)
+  with open('cities.csv', newline='') as csvfile:
+      reader = csv.reader(csvfile, delimiter=',')
       next(reader)
       for row in reader:
           rows.append(row)
       for row in rows[1:]:
           cities.append(City(row[0], float(row[3]), float(row[4])))
     
-  return cities
+      return cities
 
 cityreader(cities)
 
